bc1eea16befc75dc652fe4b5340b3f12a925f592
MarioQuimetMed <mario.medina@ficct.uagrm.edu.bo>
2023-10-20 19:07:41 -0400

    Actualizacion1

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -4,6 +4,8 @@ import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{
   
+  final ScrollController chatScrollController = ScrollController();
+
    List<Message> messageList = [
     Message(text: 'Hola amor!',fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo?',fromWho: FromWho.me),
@@ -17,7 +19,16 @@ class ChatProvider extends ChangeNotifier{
     messageList.add(newMessage);
 
     notifyListeners();
+    moveScrollToBotton();
 
+  }
+
+  void moveScrollToBotton(){
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeInOut,
+    );
   }
 
 
